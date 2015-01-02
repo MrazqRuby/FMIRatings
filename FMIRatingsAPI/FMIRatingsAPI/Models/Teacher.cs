9bf4f351a5f9cba8eff14e63937ec7d47731638b
CTAMO <johndoe2016@abv.bg>
2015-01-02 19:52:46 +0200

    Added Teacher.cs and TeacherInCourse.cs files

---
+++ FMIRatingsAPI/FMIRatingsAPI/Models/Teacher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace FMIRatingsAPI.Models
+{
+	public class Teacher
+	{
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public int Id { get; set; }
+		public string Name { get; set; } 
+
+		public virtual ICollection<TeacherInCourse> Courses { get; set; }
+		public virtual ICollection<CommentForTeacher> Comments { get; set; }
+	}
+}
---
+++ FMIRatingsAPI/FMIRatingsAPI/Models/TeacherInCourse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FMIRatingsAPI.Models
+{
+	public class TeacherInCourse
+	{
+		[Key]
+		public int Id { get; set; }
+		public int TeacherId { get; set; }
+		public int CourseId { get; set; } 
+
+		public Course Course { get; set; }
+		public Teacher Teacher { get; set; }
+	}
+}
