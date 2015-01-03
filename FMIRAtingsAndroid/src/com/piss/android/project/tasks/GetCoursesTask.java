60e14d2ca7967af444acef975d71aa0ae0a5bc68
gergana <gvasilevaa@gmail.com>
2015-01-03 18:15:17 +0200

    Add Login and GetCourses tasks

---
+++ FMIRAtingsAndroid/src/com/piss/android/project/tasks/GetCoursesTask.java
@@ -0,0 +1,55 @@
+package com.piss.android.project.tasks;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import com.piss.android.project.models.Course;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class GetCoursesTask extends AsyncTask<Void, Void, ArrayList<Course>> {
+
+	@Override
+	protected ArrayList<Course> doInBackground(Void... params) {
+		String request = "";
+		HttpClient client = new DefaultHttpClient();
+		Log.e("url", request);
+		HttpGet get = new HttpGet(request);
+
+		HttpResponse response;
+		ArrayList<Course> mList;
+		try {
+			response = client.execute(get);
+
+			String jsonResponse = EntityUtils.toString(response.getEntity());
+			JSONArray json = new JSONArray(jsonResponse);
+
+			mList = Course.parseFromJSON(json);
+
+		} catch (ClientProtocolException e) {
+
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+
+			e.printStackTrace();
+			return null;
+		} catch (JSONException e) {
+
+			e.printStackTrace();
+			return null;
+		}
+		return mList;
+	}
+
+}
