e76ccb124a1069abbf8dc1cbe366d96c6cf0059e
mimikrmvrr <mimi_9204@abv.bg>
2015-01-08 22:40:39 +0200

    Initialize basic angular app

--- WebUI/FMIRatingsWeb/public_html/js/controller.js
+++ WebUI/FMIRatingsWeb/public_html/js/controller.js
@@ -1,12 +1,24 @@
 var fmiRatingsApp = angular.module('fmiRatingsApp', []);
 
-fmiRatingsApp.controller('HomeCtrl', function ($scope) {
-  $scope.cources = [
-    {'name': 'Analiz',
-     'id': '1'},
-    {'name': 'Diskretni strukturi',
-     'id': '2'},
-    {'name': 'Geometriq',
-     'id': '3'}
-  ];
+fmiRatingsApp.controller('CourcesCtrl', function ($scope) {
+  $scope.cources = [[
+	  {
+	    "id": 1,
+	    "name": "sample string 2",
+	    "description": "sample string 3",
+	    "teachers": [
+	      "sample string 1",
+	      "sample string 2"
+	    ]
+	  },
+	  {
+	    "id": 1,
+	    "name": "sample string 2",
+	    "description": "sample string 3",
+	    "teachers": [
+	      "sample string 1",
+	      "sample string 2"
+	    ]
+	  }
+	];
 });
