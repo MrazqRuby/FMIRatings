9afb09478d4ada8afc4fd5f3065f40b58cd1058f
Siyana <siyanaslavova@gmail.com>
2015-01-10 15:22:30 +0200

    add headers to register request

--- WebUI/FMIRatingsWeb/public_html/js/registration-controller.js
+++ WebUI/FMIRatingsWeb/public_html/js/registration-controller.js
@@ -7,23 +7,29 @@
 $(document).ready(function () {
     var fmiRatingsApp = angular.module('registration', []);
 
-    fmiRatingsApp.controller("RegistrationCtrl", ["$scope", function ($scope) {
+    fmiRatingsApp.controller("RegistrationCtrl", ["$scope", "$http", function ($scope, $http) {
             $scope.formInfo = {};
-            var parameters = $scope.formInfo.Name + $scope.formInfo.Passwod;
+//            var parameters = $scope.formInfo.Name + $scope.formInfo.Passwod;
+var parameters = {'name':'aaa','password':'aaa'};
+            console.log(parameters);
             $scope.saveData = function () {
-                fmiRatingsApp.factory(function(){
-                     $scope.persons = $http({
-                    url: 'http://95.111.16.46:6420/api/Users',
+//                fmiRatingsApp.factory(function(){
+                var headers = {'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT',
+                    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'};
+                $http({
+                    url: 'http://95.111.16.46:6420/api/users',
                     method: "POST",
                     data: parameters,
-                    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                    headers: headers
                 }).success(function (data, status, headers, config) {
-                    $scope.data = data; // how do pass this to $scope.persons?
+                    $scope.data = data;
                 }).error(function (data, status, headers, config) {
                     $scope.status = status;
                 });
-                });
-               
+//                });
+//               
             };
 
 
