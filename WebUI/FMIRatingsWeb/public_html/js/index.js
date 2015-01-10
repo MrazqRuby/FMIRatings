d95a2f0edb86c4c27027db0573ac3633f3f94fe8
Siyana <siyanaslavova@gmail.com>
2015-01-10 17:18:53 +0200

    add index.js

---
+++ WebUI/FMIRatingsWeb/public_html/js/index.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function () {
+    
+//    localStorage.setItem("authentication", "dXNlcjp1c2Vy");
+    
+//    if(localStorage.getItem("authentication")){
+//        $("html").load("home-page.html");
+//    } else {
+//        $("html").load("nonregistered-user-homepage.html");
+//    }
+    
+    });
+
+
