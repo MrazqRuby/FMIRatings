d25a0fdd4681e4f391b157c43c04dc3a0164b0d9
skeleta <ivankapukaranov@gmail.com>
2015-01-10 17:06:28 +0200

    APIConnectionConstants.java

--- FMIRAtingsAndroid/src/com/piss/android/project/utils/APIConnectionConstants.java
+++ FMIRAtingsAndroid/src/com/piss/android/project/utils/APIConnectionConstants.java
@@ -16,7 +16,6 @@ public class APIConnectionConstants {
 	public final static String API_TEACHERS_COMMENTS="TeacherComments";
 	public final static String API_REGISTRATION = "Users/PostUser";
 	public final static String API_LOGIN = "Users";
-<<<<<<< HEAD
 	public final static String COURSE_ID = "CourseId";
 	public final static String USER_ID = "UserId";
 	public final static String CLARITY = "Clarity";
@@ -26,9 +25,7 @@ public class APIConnectionConstants {
 	public final static String USEFULNESS = "Usefulness";
 	public final static String COMMENT = "Comment";
 	
-=======
 	public final static String API_VOTE_FOR_TEACHER = "VoteForTeacher";
->>>>>>> 9376ce7eb3b73fb796b303c077d2641d9dfc984f
 	
 	/* API RESPONSE CONSTANTS */
 	public final static String ID = "id";
@@ -44,12 +41,9 @@ public class APIConnectionConstants {
 	
 	public final static String COMMENT_TEXT = "text";
 	
-	public final static String USER_ID = "userId";
-	public final static String CLARITY = "clarity";
 	public final static String ENTHUSUASM = "enthusiasm";
 	public final static String EVALUATION = "evaluation";
 	public final static String SPEED = "speed";
 	public final static String SCOPE = "scope";
-	public final static String COMMENT = "comment";
 	
 }
