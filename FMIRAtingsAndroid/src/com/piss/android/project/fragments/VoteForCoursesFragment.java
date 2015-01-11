57f76d79ef47d27888a8ebecdbe8271cd742d27e
Polina Koleva <polly.akoleva@gmail.com>
2015-01-11 21:31:54 +0200

    Fixed vote for courses

--- FMIRAtingsAndroid/src/com/piss/android/project/fragments/VoteForCoursesFragment.java
+++ FMIRAtingsAndroid/src/com/piss/android/project/fragments/VoteForCoursesFragment.java
@@ -41,56 +41,56 @@ public class VoteForCoursesFragment extends Fragment{
 	public View onCreateView(LayoutInflater inflater,
 			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.vote_for_course_layout, null);
-
-		ratingBarClarity = (RatingBar) rootView
-				.findViewById(R.id.clarity_rating);
-		ratingBarWorkload = (RatingBar) rootView
-				.findViewById(R.id.workload_rating);
-		ratingBarInterest = (RatingBar) rootView
-				.findViewById(R.id.interest_rating);
-		ratingBarSimplicity = (RatingBar) rootView.findViewById(R.id.simplicity_rating);
-		ratingBarUsefulness = (RatingBar) rootView.findViewById(R.id.usefulness_rating);
-		 ratingBarClarity.setOnRatingBarChangeListener(new OnRatingBarChangeListener() {
-		        public void onRatingChanged(RatingBar ratingBar, float rating,
-		         boolean fromUser) {
-
-		         Toast.makeText(getActivity(),"Your Selected Ratings  : " + String.valueOf(rating),Toast.LENGTH_LONG).show();
-
-		        }
-		       });
-		
-		comment = (TextView) rootView.findViewById(R.id.editComment);
-		sendComment = (ImageView) rootView.findViewById(R.id.commentArrow);
-		sendComment.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				SharedPreferences settings = getActivity().getSharedPreferences(APIConnectionConstants.PREFERENCES, Activity.MODE_PRIVATE);
-				String auth = settings.getString(APIConnectionConstants.AUTHENTICATION, "");
-
-				String what = "VoteForCourse";
-				int courseId = getArguments().getInt(COURSE_ID);
-				int userId = 0;
-				int ratingClarity = (int) ratingBarClarity.getRating();
-				int ratingWorkload = (int) ratingBarWorkload.getRating();
-				int ratingInterest = (int) ratingBarInterest.getRating();
-				int ratingSimplicity = (int) ratingBarSimplicity.getRating();
-				int ratingUsefulness = (int) ratingBarUsefulness.getRating();
-				String text = comment.getText().toString();
-
-				PostVoteForCourseTask postVote = new PostVoteForCourseTask(what,
-						courseId, userId, ratingClarity, ratingWorkload,
-						ratingInterest, ratingSimplicity, ratingUsefulness, text){
-				
-				@Override
-				protected void onPostExecute(Boolean result) {
-					Toast.makeText(getActivity(), "Result" + result, Toast.LENGTH_LONG).show();
-				}};
-
-				postVote.execute();
-
-			}
-		});
+//
+//		ratingBarClarity = (RatingBar) rootView
+//				.findViewById(R.id.clarity_rating);
+//		ratingBarWorkload = (RatingBar) rootView
+//				.findViewById(R.id.workload_rating);
+//		ratingBarInterest = (RatingBar) rootView
+//				.findViewById(R.id.interest_rating);
+//		ratingBarSimplicity = (RatingBar) rootView.findViewById(R.id.simplicity_rating);
+//		ratingBarUsefulness = (RatingBar) rootView.findViewById(R.id.usefulness_rating);
+//		 ratingBarClarity.setOnRatingBarChangeListener(new OnRatingBarChangeListener() {
+//		        public void onRatingChanged(RatingBar ratingBar, float rating,
+//		         boolean fromUser) {
+//
+//		         Toast.makeText(getActivity(),"Your Selected Ratings  : " + String.valueOf(rating),Toast.LENGTH_LONG).show();
+//
+//		        }
+//		       });
+//		
+//		comment = (TextView) rootView.findViewById(R.id.editComment);
+//		sendComment = (ImageView) rootView.findViewById(R.id.commentArrow);
+//		sendComment.setOnClickListener(new View.OnClickListener() {
+//
+//			@Override
+//			public void onClick(View v) {
+//				SharedPreferences settings = getActivity().getSharedPreferences(APIConnectionConstants.PREFERENCES, Activity.MODE_PRIVATE);
+//				String auth = settings.getString(APIConnectionConstants.AUTHENTICATION, "");
+//
+//				String what = "VoteForCourse";
+//				int courseId = getArguments().getInt(COURSE_ID);
+//				int userId = 0;
+//				int ratingClarity = (int) ratingBarClarity.getRating();
+//				int ratingWorkload = (int) ratingBarWorkload.getRating();
+//				int ratingInterest = (int) ratingBarInterest.getRating();
+//				int ratingSimplicity = (int) ratingBarSimplicity.getRating();
+//				int ratingUsefulness = (int) ratingBarUsefulness.getRating();
+//				String text = comment.getText().toString();
+//
+//				PostVoteForCourseTask postVote = new PostVoteForCourseTask(what,
+//						courseId, userId, ratingClarity, ratingWorkload,
+//						ratingInterest, ratingSimplicity, ratingUsefulness, text){
+//				
+//				@Override
+//				protected void onPostExecute(Boolean result) {
+//					Toast.makeText(getActivity(), "Result" + result, Toast.LENGTH_LONG).show();
+//				}};
+//
+//				postVote.execute();
+//
+//			}
+//		});
 
 		return rootView;
 	}
