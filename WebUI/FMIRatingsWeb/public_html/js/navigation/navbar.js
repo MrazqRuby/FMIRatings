689fff47b07fe348791aa4a4b1581f6c6a2db502
mimikrmvrr <mimi_9204@abv.bg>
2015-01-11 18:34:57 +0200

    add upload file controller

--- WebUI/FMIRatingsWeb/public_html/js/navigation/navbar.js
+++ WebUI/FMIRatingsWeb/public_html/js/navigation/navbar.js
@@ -134,7 +134,9 @@ app.controller('disciplineDetailsController', function ($rootScope, $scope, $rou
         if (typeof $scope.rating !== 'undefined') {
             $scope.rating.CourseId = $scope.dataCourse.id;
             var rating = $scope.rating;
-            
+
+            var auth = "Basic " + localStorage.getItem("authentication");
+            console.log(auth);
             $http.defaults.headers.common.Authorization = localStorage.getItem("authentication");
             $http({
                 url: 'http://95.111.16.46:6420/api/VoteForCourse',
@@ -146,7 +148,7 @@ app.controller('disciplineDetailsController', function ($rootScope, $scope, $rou
 //                headers: {"Authentication": localStorage.getItem("authentication")}
             }).success(function (data) {
                 debugger
-                $scope.result = data;
+                $scope.departments = data;
                 alert("Вашият глас е запазен.")
             }).error(function (data) {
                 debugger
@@ -244,13 +246,68 @@ app.controller('teacherDetailsController', function ($rootScope, $scope, $routeP
         }
     }
 
-
 //    if ($scope.votes.length > 0) {
 //        $scope.avarage = voteSum / $scope.votes.length;
 //    }
 
 
 });
+
+app.controller('FileUploadController', function($scope) {
+    $scope.setFiles = function(element) {
+        $scope.$apply(function(scope) {
+        console.log('files:', element.files);
+        // Turn the FileList object into an Array
+        $scope.files = []
+        for (var i = 0; i < element.files.length; i++) {
+          $scope.files.push(element.files[i])
+        }
+        $scope.progressVisible = false
+        });
+    };
+
+    $scope.uploadFile = function() {
+        var fd = new FormData()
+        for (var i in $scope.files) {
+            fd.append("uploadedFile", $scope.files[i])
+        }
+        var xhr = new XMLHttpRequest()
+        xhr.upload.addEventListener("progress", uploadProgress, false)
+        xhr.addEventListener("load", uploadComplete, false)
+        xhr.addEventListener("error", uploadFailed, false)
+        xhr.addEventListener("abort", uploadCanceled, false)
+        xhr.open("POST", "/fileupload")
+        $scope.progressVisible = true
+        xhr.send(fd)
+    }
+
+    function uploadProgress(evt) {
+        $scope.$apply(function(){
+            if (evt.lengthComputable) {
+                $scope.progress = Math.round(evt.loaded * 100 / evt.total)
+            } else {
+                $scope.progress = 'unable to compute'
+            }
+        })
+    }
+
+    function uploadComplete(evt) {
+        /* This event is raised when the server send back a response */
+        alert(evt.target.responseText)
+    }
+
+    function uploadFailed(evt) {
+        alert("There was an error attempting to upload the file.")
+    }
+
+    function uploadCanceled(evt) {
+        $scope.$apply(function(){
+            $scope.progressVisible = false
+        })
+        alert("The upload has been canceled by the user or the browser dropped the connection.")
+    }
+});
+
 //
 //app.controller("MainCtrl",["$scope", "$location", function($scope, $location) {
 //  $scope.menuClass = function(page) {
